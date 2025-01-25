b489c037858a032a3c848a0311391f0929762e27
sogeking-d-god <ronen.vol@gmail.com>
2025-01-25 14:46:34 +0200

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import sys
 import os
-
+import zlib
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -16,6 +16,13 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Initialized git directory")
+
+    elif command == "cat-file" and sys.argv[2] == "-p":
+        obj_name = sys.argv[3]
+        with open(f".git/objects/{obj_name[:2]}/{obj_name[2:]}", "rb") as f:
+            raw = zlib.decompress(f.read())
+            header, content = raw.split(b"\0", maxsplit=1)
+            print(content.decode(encoding="utf-8"), end="")
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
